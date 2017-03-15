86f53ebfae829533e9fe0a78e4bd96be3bf8c197
kenneth.marete <maretekent@gmail.com>
2017-03-15 17:27:58 +0300

    added blog files and defualt django admin

---
+++ project_x/personal/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    return render(request, 'personal/home.html')
+
+def contact(request):
+    return render(request, 'personal/contact.html',
+                  {'content':['if you would like to contact me, '
+                              'please email me kentccs@gmail.com']})
